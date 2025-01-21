d8070e3f8dbe22c8c6e0e51fad42ca1a4ddd9d3c
Hoimn <737753@student.dusd.net>
2025-01-21 11:30:11 -0800

    NavMEsh
    NavMEsh

--- AIGonzalezP3/Assets/FollowWaypoint.cs
+++ AIGonzalezP3/Assets/FollowWaypoint.cs
@@ -1,27 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowWaypoint : MonoBehaviour
 {
-   
-    Transform goal;
-    public float speed = 5.0f;
-    float accuracy = 2.0f;
-    public float rotSpeed = 5.0f;
-
     public GameObject wpManager;
     GameObject[] waypoints;
     GameObject currentNode;
-    public int currentWP = 0;
-    Graph g;
+    NavMeshAgent agent;
+
 
     // Start is called before the first frame update
     void Start()
     {
         waypoints = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<WPManager>().graph;
         currentNode = waypoints[0];
+        Time.timeScale = 5;
+
+        agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
@@ -32,37 +29,19 @@ public class FollowWaypoint : MonoBehaviour
 
     public void GoToRuin()
     {
-        g.AStar(currentNode, waypoints[3]);
-        currentWP = 0;
+        //g.AStar(currentNode, waypoints[3]);
+        agent.SetDestination(waypoints[3].transform.position);
     }
 
 
     public void GoToHeli()
     {
-        g.AStar(currentNode, waypoints[13]);
-        currentWP = 0;
+        //g.AStar(currentNode, waypoints[13]);
+        agent.SetDestination(waypoints[13].transform.position);
     }
 
     public void LateUpdate()
     {
-        if (g.pathList.Count == 0 || currentWP == g.pathList.Count) { return; }
-
-        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy)
-        {
-            currentNode = g.pathList[currentWP].getId();
-            currentWP++;
-        }
-
-        if (currentWP < g.pathList.Count)
-        {
-            goal = g.pathList[currentWP].getId().transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
-            Vector3 direction = lookAtGoal - this.transform.position;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-
-            this.transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-
-
+     
     }
 }
